7af3c6ba7ed71a9d8c95b45781e61a396dc78484
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-15 19:20:28 +1300

    Added back sheet.

--- index.es.js
+++ index.es.js
@@ -14,6 +14,12 @@ export { default as DocumentPage } from './src/document/DocumentPage'
 export { default as DocumentConfigurator } from './src/document/DocumentConfigurator'
 export { documentConversion }
 
+export { default as SheetDocument } from './src/sheet/model/SheetDocument'
+export { default as SheetNode } from './src/sheet/model/SheetNode'
+export { default as SheetEditor } from './src/sheet/SheetEditor'
+export { default as SheetConfigurator } from './src/sheet/SheetConfigurator'
+export { sheetConversion }
+
 export { default as JsContext } from './src/js-context/JsContext'
 export { default as functions } from './src/js-context/functions'
 
--- make.js
+++ make.js
@@ -121,7 +121,7 @@ function buildStencilaNodeJS() {
 function buildExamples() {
   b.copy('./examples/*/*.html', './build/')
   //
-  ;['document', 'dashboard'].forEach((example) => {
+  ;['document', 'dashboard', 'sheet'].forEach((example) => {
     b.js(`examples/${example}/app.js`, {
       dest: `build/examples/${example}/app.js`,
       format: 'umd', moduleName: `${example}Example`,
